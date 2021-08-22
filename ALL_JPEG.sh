7b19838654c169b2714f2e5a55b4a4507622e346
Roman Adereyko <romaadereyko@gmail.com>
2021-08-23 00:44:42 +0300

    Output for find all JPEGs

--- ALL_JPEG.sh
+++ ALL_JPEG.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-find /opt/hellthrash/images/*.jpeg -size +400k -mmin 60
+TIMESTUMP=$(date "+%H:%M:%S %d.%m.%Y")
+find /opt/hellthrash/images/*.jpeg -size +400k -mmin 60 > /opt/hellthrash/periodic/run_$TIMESTAMP.txt
 
 exit 0
